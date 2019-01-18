0d2628d54d90005d3c6bcb1d4f411e237525552e
BillyPittman1993 <billytrading93@gmail.com>
2019-01-18 00:28:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  } 
+  if (someValue >= 2000) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  if (someValue >= 2500) 
+  {
+    result = "No can do.";
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  case 'generous': {
+    result = 'Thank you so much.';
+    break;
+    }
+  case 'not as generous': {
+    result = 'Thank you.';
+    break;
+    }
+  case 'thanks for everything': {
+    result = 'Bye.';
+    break;
+    }
+  }
+  return result
+}
